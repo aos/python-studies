a713e442a76ed52b7fa54c87f1ac1f54225bbb7a
Aos <aosdab@gmail.com>
2017-07-07 12:56:36 -0400

    Done ch3 python

--- ch3.py
+++ ch3.py
@@ -7,21 +7,116 @@ float('3.1415') # casts to float
 str(32) # casts to string
 
 import math # imports math module object
-ratio = signal_power / noise_power
-decibels = 10 * math.log10(ratio)
+# ratio = signal_power / noise_power
+# decibels = 10 * math.log10(ratio)
 
 radians = 0.7
 height = math.sin(radians)
 
 # Composition
-x = math.sin(degrees / 360 * 2 * math.pi)
+# x = math.sin(degrees / 360 * 2 * math.pi)
 
 # Function definitions
 def print_lyrics():
-  print("I'm a lumberjack, and I'm OK.")
-  print("I sleep all night and I work all day")
+    print("I'm a lumberjack, and I'm OK.")
+    print("I sleep all night and I work all day")
 
 def repeat_lyrics():
-  
-  
+    print_lyrics()
+    print_lyrics()
 
+# repeat_lyrics()
+
+def right_justify(string):
+    repeat = 70 - len(string) 
+    print((" " * repeat) + string)
+
+# right_justify('aos')
+
+def do_twice(f, v1, v2):
+    f(v1, v2)
+    f(v1, v2)
+
+def print_twice(string):
+    print(string)
+    print(string)
+
+# do_twice(print_twice, 'hello')
+
+def do_four(f, value):
+    do_twice(f, value)
+    do_twice(f, value)
+
+# do_four(print, 'hello')
+
+def draw_plus():
+    print('+', end=' ')
+
+def draw_hline():
+    print('- ' * 4, end='')
+
+def draw_vline():
+    print('|', end='')
+    print(' ' * 8, end=' ')
+    print('|', end='')
+    print(' ' * 8, end=' ')
+    print('|', end='')
+    print(' ' * 8, end=' ')
+    print('|', end='')
+    print(' ' * 8, end=' ')
+    print('|')
+    
+def alternate_once(f_one, f_two):
+    f_one()
+    f_two()
+
+def do_four(f):
+    f()
+    f()
+    f()
+    f()
+
+
+def draw_grid():
+    alternate_once(draw_plus, draw_hline)
+    alternate_once(draw_plus, draw_hline)
+    draw_plus()
+    print()
+    do_four(draw_vline)
+    alternate_once(draw_plus, draw_hline)
+    alternate_once(draw_plus, draw_hline)
+    draw_plus()
+    print()
+    do_four(draw_vline)
+    alternate_once(draw_plus, draw_hline)
+    alternate_once(draw_plus, draw_hline)
+    draw_plus()
+    print()
+
+def draw_big():
+    do_twice(alternate_once, draw_plus, draw_hline)
+    do_twice(alternate_once, draw_plus, draw_hline)
+    draw_plus()
+    print()
+    do_four(draw_vline)
+    do_twice(alternate_once, draw_plus, draw_hline)
+    do_twice(alternate_once, draw_plus, draw_hline)
+    draw_plus()
+    print()
+    do_four(draw_vline)
+    do_twice(alternate_once, draw_plus, draw_hline)
+    do_twice(alternate_once, draw_plus, draw_hline)
+    draw_plus()
+    print()
+    do_four(draw_vline)
+    do_twice(alternate_once, draw_plus, draw_hline)
+    do_twice(alternate_once, draw_plus, draw_hline)
+    draw_plus()
+    print()
+    do_four(draw_vline)
+    do_twice(alternate_once, draw_plus, draw_hline)
+    do_twice(alternate_once, draw_plus, draw_hline)
+    draw_plus()
+    print()
+
+draw_big()
