bd93d2d19155c1fe9d2a415bfe01c3c87affac07
Aos <aosdab@gmail.com>
2017-07-06 21:49:07 -0400

    Python: starting ch3

---
+++ ch3.py
@@ -0,0 +1,27 @@
+## Chapter 3 -- Functions
+
+type(42) # a function call
+
+int('32') # casts to int
+float('3.1415') # casts to float
+str(32) # casts to string
+
+import math # imports math module object
+ratio = signal_power / noise_power
+decibels = 10 * math.log10(ratio)
+
+radians = 0.7
+height = math.sin(radians)
+
+# Composition
+x = math.sin(degrees / 360 * 2 * math.pi)
+
+# Function definitions
+def print_lyrics():
+  print("I'm a lumberjack, and I'm OK.")
+  print("I sleep all night and I work all day")
+
+def repeat_lyrics():
+  
+  
+
